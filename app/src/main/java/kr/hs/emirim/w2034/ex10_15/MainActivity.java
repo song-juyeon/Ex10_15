93a3e6d288f48924e37a19bf9a4e94656b76c200
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-20 13:49:13 +0900

    completion 직접풀어보기 10_3 commit

---
+++ app/src/main/java/kr/hs/emirim/w2034/ex10_15/MainActivity.java
@@ -0,0 +1,67 @@
+package kr.hs.emirim.w2034.ex10_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edit1;
+    EditText edit2;
+    RadioGroup rg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(R.string.title_main);
+
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+        rg = findViewById(R.id.rg);
+
+        Button btnPlus = findViewById(R.id.btn_plus);
+
+        btnPlus.setOnClickListener(btnListener);
+    }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            char op = 0;
+            switch (rg.getCheckedRadioButtonId()){
+                case  R.id.radio_plus:
+                    op = '+';
+                    break;
+                case  R.id.radio_minus:
+                    op = '-';
+                    break;
+                case  R.id.radio_multi:
+                    op = '*';
+                    break;
+                case  R.id.radio_divide:
+                    op = '/';
+                    break;
+            }
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            intent.putExtra("num1", Integer.parseInt(edit1.getText().toString()));
+            intent.putExtra("num2", Integer.parseInt(edit2.getText().toString()));
+            intent.putExtra("op", op);
+            startActivityForResult(intent,0);
+        }
+    };
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            int result = data.getIntExtra("result", 0);
+            Toast.makeText(getApplicationContext(), "합계: "+result, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/kr/hs/emirim/w2034/ex10_15/SecondActivity.java
@@ -0,0 +1,49 @@
+package kr.hs.emirim.w2034.ex10_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    int result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int num1 = intent.getIntExtra("num1", 0);
+        int num2 = intent.getIntExtra("num2", 0);
+        char op = intent.getCharExtra("op", '0');
+        switch (op){
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+
+        }
+        Button btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(btnListener);
+    }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+            mainIntent.putExtra("result", result);
+            setResult(RESULT_OK, mainIntent);
+            finish();
+        }
+    };
+}
